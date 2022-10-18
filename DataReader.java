63b083350d0cd9c04b6fd1517655c01ce512ccda
kjmmjk43 <kjmmjk43@gmail.com>
2022-10-18 14:26:32 -0400

    data stubs

--- DataReader.java
+++ DataReader.java
@@ -1,3 +1,8 @@
 public class DataReader {
     
+
+    public void getAllUsers(){
+
+        
+    }
 }
---
+++ DataWriter.java
@@ -0,0 +1,6 @@
+public class DataWriter {
+ 
+    public void saveAllUsers(){
+        
+    }
+}
