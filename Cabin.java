c77346ce5a3f1bc55ce113f4292aa4c84684741a
kjmmjk43 <kjmmjk43@gmail.com>
2022-10-18 14:22:51 -0400

    some classes

--- Cabin.java
+++ Cabin.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Cabin {
+    public ArrayList<String> schedule = new ArrayList<String>();
+    public Random rand = new Random(); 
+    public ArrayList<String> counselors = new ArrayList<String>();
+    public ArrayList<Campers> camper = new ArrayList<Campers>();
+
     
 
     
---
+++ Campers.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Campers extends User{
+    public String name;
+    public double age;
+    public ArrayList<String> allergies = new ArrayList<String>();
+    public Gender gender;
+    //public ArrayList<EmergencyContacts> emergencyContact = new ArrayList<EmergencyContacts>();
+    public ArrayList<String> cabin = new ArrayList<String>();
+
+    public void viewScedule(){
+
+    }
+
+    public void contactCounselor(){
+        
+    }
+    
+}
