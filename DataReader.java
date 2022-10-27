dc981f551b85e513232225e80ee89a1bb90b0e0c
kjmmjk43 <kjmmjk43@gmail.com>
2022-10-27 14:23:47 -0400

    some json files

--- DataReader.java
+++ DataReader.java
@@ -1,12 +1,51 @@
-import java.util.ArrayList;
-
-public class DataReader {
-    
-
-    public boolean getAllUsers(ArrayList<User> users){
-            
-
-        return true;
-        
+ 
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+ 
+public class DataReader
+{
+    public static void main(String[] args) 
+    {
+        //JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+         
+        try (FileReader reader = new FileReader("employees.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+ 
+            JSONArray employeeList = (JSONArray) obj;
+            System.out.println(employeeList);
+             
+            //Iterate over employee array
+            employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
+ 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    private static void parseEmployeeObject(JSONObject employee) 
+    {
+        //Get employee object within list
+        JSONObject employeeObject = (JSONObject) employee.get("employee");
+         
+        //Get employee first name
+        String firstName = (String) employeeObject.get("firstName");    
+        System.out.println(firstName);
+         
+        //Get employee last name
+        String lastName = (String) employeeObject.get("lastName");  
+        System.out.println(lastName);
+         
+        //Get employee website name
+        String website = (String) employeeObject.get("website");    
+        System.out.println(website);
     }
 }
